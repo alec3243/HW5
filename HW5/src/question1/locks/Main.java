3b53640c0cb2f188cc37bd3dbad6832385a09e45
alec3243 <alec3243@gmail.com>
2018-11-03 16:08:44 -0700

    Initial commit, locks

---
+++ HW5/src/question1/locks/Main.java
@@ -0,0 +1,10 @@
+package question1.locks;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HW5/src/question1/locks/Producer.java
@@ -0,0 +1,5 @@
+package question1.locks;
+
+public class Producer {
+
+}
